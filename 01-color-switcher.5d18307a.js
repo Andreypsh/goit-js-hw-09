3a89344c1502b7a350468caf0cd4a8941173b291
Andreypsh <116453034+Andreypsh@users.noreply.github.com>
2023-08-18 15:41:05 +0000

    Deploying to gh-pages from @ Andreypsh/goit-js-hw-09@3b7213c0d8591f4398cc26d4bab5c3a25dd2bc04 🚀

---
+++ 01-color-switcher.5d18307a.js
@@ -0,0 +1,2 @@
+let t=null;refs={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")},refs.stopBtn.disabled=!0,refs.startBtn.addEventListener("click",(function(){refs.stopBtn.disabled=!1,refs.startBtn.disabled=!0,t=setInterval((()=>{refs.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),refs.stopBtn.addEventListener("click",(function(){refs.stopBtn.disabled=!0,refs.startBtn.disabled=!1,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.5d18307a.js.map
